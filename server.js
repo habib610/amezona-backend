a2d2137d8644c5f317ebe151714466b0cdbf712b
habib610 <habib54562@gmail.com>
2020-12-27 14:58:48 +0600

    seed api for product and all products

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import data from  './data.js';
+import productRouter from './Router/productRouter.js';
 import userRouter from './Router/userRouter.js';
 
 const app = express();
@@ -16,20 +16,9 @@ app.get('/', (req, res)=> {
     res.send('Server is ready');
 })
 
-app.get('/api/products/:id', (req, res)=> {
-    const product = data.products.find(prd => prd._id === req.params.id);
-    console.log(product)
-    if(product) {
-        res.send(product)
-    } else {
-        res.status(404).send({message: "Product not Found"})
-    }
-})
 
-app.get('/api/products', (req, res)=> {
-    res.send(data.products)
-})
 
+app.use('/api/products', productRouter)
 
 app.use('/api/users', userRouter);
 
