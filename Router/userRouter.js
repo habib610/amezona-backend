cbdb2e47ddb78a47fc78b6e8dc8538643a0f8596
habib610 <habib54562@gmail.com>
2020-12-27 20:08:56 +0600

    generating-jwt

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
+import bcrypt from 'bcryptjs';
 import data from '../data.js';
 import User from '../Models/userModel.js';
+import { generateToken } from '../utils/utils.js';
 
 
 
@@ -11,6 +13,26 @@ userRouter.get('/seed', expressAsyncHandler(async (req, res)=> {
     // await User.remove({});
     const createdUser = await User.insertMany(data.users)
     res.send({createdUser})
+}))
+
+userRouter.post('/signin', expressAsyncHandler(async(req, res)=> {
+    const user = await User.findOne({email: req.body.email});
+    if(user) {
+        if(bcrypt.compareSync(req.body.password, user.password)){
+            res.send({
+                _id: user._id,
+                name: user.name,
+                password: user.password,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user)
+            })
+        }
+    }
+    else {
+        res.status(401).send({message: "Invalid Email or Password"})
+    }
+
 }))
 
 export default userRouter;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import productRouter from './Router/productRouter.js';
 import userRouter from './Router/userRouter.js';
 
+dotenv.config();
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
